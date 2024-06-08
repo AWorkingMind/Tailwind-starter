69b9334d450016451d1a80a0d7cf9e6019ffe137
Christian Rasmussen <chrtobias4780@gmail.com>
2024-06-08 13:13:03 +0200

    basic start

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'custom-yellow': '#d68c09',
+      'custom-black': '#181818',
+    },
   },
   plugins: [],
 }
